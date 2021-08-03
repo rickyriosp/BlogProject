cc929597c1b29065500d22a01c42bc636ae767ee
Ricky Rios <riki.rios91@gmail.com>
2021-08-03 18:15:25 -0400

    added tag model class

---
+++ Models/Tag.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        // Navigation properties
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser Author { get; set; }
+    }
+}

2516073aff99b18defa5e07f58053c16a215b287
Ricky Rios <riki.rios91@gmail.com>
2021-08-04 23:54:28 -0400

    added sql server migration

--- Data/Migrations/20210804231507_001.Designer.cs
+++ Data/Migrations/20210804231507_001.Designer.cs
@@ -4,14 +4,16 @@ using BlogProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210804231507_001")]
+    partial class _001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

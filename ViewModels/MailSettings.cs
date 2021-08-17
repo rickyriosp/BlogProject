14cd13dfaca02c848f5c492a082c270cb546fa4c
Ricky Rios <riki.rios91@gmail.com>
2021-08-16 22:04:45 -0400

    created MailSettings model

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.ViewModels
+{
+    public class MailSettings
+    {
+        // Configure and use SMTP server from Google for example
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}

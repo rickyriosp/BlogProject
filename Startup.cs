b8702d54557bbe3b9271eccff22a59dece61cae8
Ricky Rios <riki.rios91@gmail.com>
2021-08-20 23:50:50 -0400

    register ISlugService

--- Startup.cs
+++ Startup.cs
@@ -59,6 +59,9 @@ namespace BlogProject
 
             // Register my Image Service
             services.AddScoped<IImageService, BasicImageService>();
+
+            // Register my Slug Service
+            services.AddScoped<ISlugService, BasicSlugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

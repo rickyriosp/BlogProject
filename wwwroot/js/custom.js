8c12fbceab5355995f1b8160f8a84e4c6a06cbdf
Ricky Rios <riki.rios91@gmail.com>
2021-09-06 12:03:11 -0400

    implemented AddTag function on Post Create

---
+++ wwwroot/js/custom.js
@@ -0,0 +1,20 @@
+﻿let index = 0;
+
+function AddTag() {
+    // Get a reference to the TagEntry input element
+    let tagEntry = document.getElementById("TagEntry");
+
+    // Create a new Select Option
+    let newOption = new Option(tagEntry.value, tagEntry.value);
+
+    document.getElementById("TagList").options[index++] = newOption;
+
+    // Clear the TagEntry control
+    tagEntry.value = '';
+
+    return true;
+}
+
+function DeleteTag() {
+
+}

183f4077fcf09c69d01602280a17d635eea191e0
Ricky Rios <riki.rios91@gmail.com>
2021-09-15 00:36:44 -0400

    added SweetAlert2 for Tag create and delete popups

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -10,13 +10,24 @@ function AddTag() {
     // Get a reference to the TagEntry input element
     let tagEntry = document.getElementById("TagEntry");
 
-    // Create a new Select Option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
+    // Use the new Search function to help detect an error state
+    let searchResult = Search(tagEntry.value);
 
-    document.getElementById("TagList").options[index++] = newOption;
+    if (searchResult != null) {
+        // Trigger Sweet Alert for the error result
+        swalWithDarkButton.fire({
+            html: `<span class="font-weight-bolder">${searchResult.toUpperCase()}</span>`
+        })
+    } else {
+        // Create a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+
+        document.getElementById("TagList").options[index++] = newOption;
+    }
 
     // Clear the TagEntry control
     tagEntry.value = "";
+    tagEntry.focus();
 
     return true;
 }
@@ -24,18 +35,24 @@ function AddTag() {
 function DeleteTag() {
     let tagCount = 1;
 
-    while (tagCount > 0) {
-        let tagList = document.getElementById("TagList")
-        let selectedIndex = tagList.selectedIndex;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
 
-        if (selectedIndex >= 0) {
-            tagList.options[selectedIndex] = null;
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: '<span class="font-weight-bolder">CHOOSE A TAG BEFORE DELETING</span>'
+        })
+
+        return true;
+    }
+
+    while (tagCount > 0) {
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         } else {
             tagCount = 0;
         }
-
-        index--;
     }
 }
 
@@ -48,3 +65,37 @@ function onSubmit () {
 }
 
 document.getElementById("Form").addEventListener("submit", onSubmit)
+
+// The Search function will detect either an empty or a duplicate Tag
+// and return an error string if an error is detected
+function Search(str) {
+    // Empty Tag
+    if (str === "") {
+        return "Empty tags are note permitted";
+    }
+
+    let tagsEl = document.getElementById("TagList");
+
+    // Duplicate Tag
+    if (tagsEl) {
+        let options = tagsEl.options;
+
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str) {
+                return `The Tag #${str} is not permitted because is a duplicate`;
+            }
+        }
+    }
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        popup: "swal2popup",
+        actions: "w-75",
+        confirmButton: 'btn btn-danger d-grid btn-outline-dark w-100',
+    },
+    imageUrl: "/assets/img/oops.png",
+    timer: 3000,
+    confirmButtonText: 'Close',
+    buttonsStyling: false,
+})

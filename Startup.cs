42b8ccb37e72231fb3b618971f6f7e76725a0bf6
Ricky Rios <riki.rios91@gmail.com>
2021-09-20 22:16:42 -0400

    add custom Route for Posts Details

--- Startup.cs
+++ Startup.cs
@@ -89,6 +89,10 @@ namespace BlogProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "SlugRoute",
+                    pattern: "BlogPosts/UrlFriendly/{slug}",
+                    defaults: new { controller = "Posts", action = "Details" });
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
